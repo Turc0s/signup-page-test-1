b9c32506f541cbcd29458e1d17e3f09a32aeff69
tapsa <tapsa872@gmail.com>
2018-02-10 15:43:53 +0200

    Added project structure

--- signup-project/src/app/app.module.ts
+++ signup-project/src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HelloPageComponent } from './hello-page/hello-page.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { MainComponent } from './main/main.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { ChangeinfoComponent } from './main/changeinfo/changeinfo.component';
+import { HellopageComponent } from './main/hellopage/hellopage.component';
+import { ShowinfoComponent } from './main/showinfo/showinfo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HelloPageComponent,
+    LoginpageComponent,
+    MainComponent,
+    SignupPageComponent,
+    ChangeinfoComponent,
+    HellopageComponent,
+    ShowinfoComponent
   ],
   imports: [
     BrowserModule
---
+++ signup-project/src/app/main/hellopage/hellopage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hellopage',
+  templateUrl: './hellopage.component.html',
+  styleUrls: ['./hellopage.component.css']
+})
+export class HellopageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
