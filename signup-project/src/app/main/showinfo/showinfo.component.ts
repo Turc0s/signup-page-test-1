75d21bd2bcec5325f294352024f2e6fffc5fedc1
tapsa <tapsa872@gmail.com>
2018-02-10 20:08:54 +0200

    Added the showinfo functionality, show database info

--- signup-project/src/app/main/showinfo/showinfo.component.ts
+++ signup-project/src/app/main/showinfo/showinfo.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../shared/user.model';
 
+import { UserInfo } from "./../../shared/user-info.model";
+
 import { UserService } from "../../shared/user.service";
 
 @Component({
@@ -17,9 +19,20 @@ export class ShowinfoComponent implements OnInit {
     new User("Tapio", "Mattila", "Turc", "test@gmail.com")
   ];
 
+  userinfoList: UserInfo[];
+
   constructor(private _userService: UserService) { }
 
   ngOnInit() {
+    var listItem = this._userService.getData();
+    listItem.snapshotChanges().subscribe(item => {
+      this.userinfoList = [];
+      item.forEach(element => {
+        var storageData = element.payload.toJSON();
+        storageData["$key"] = element.key;
+        this.userinfoList.push(storageData as UserInfo);
+      });
+    });
   }
 
 }
