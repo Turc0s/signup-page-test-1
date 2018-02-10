d36b9e4ad99e2a50154a8490e9724e41e2b6d2ef
tapsa <tapsa872@gmail.com>
2018-02-10 17:57:39 +0200

    Added reset functionality and validation to form

--- signup-project/src/app/main/changeinfo/changeinfo.component.ts
+++ signup-project/src/app/main/changeinfo/changeinfo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { UserService } from "../../shared/user.service";
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-changeinfo',
@@ -12,6 +13,23 @@ export class ChangeinfoComponent implements OnInit {
   constructor(private _userService: UserService) { }
 
   ngOnInit() {
+    this.resetForm();
+  }
+
+  onSubmit(userinfoForm: NgForm) {
+
+  }
+
+  resetForm(userinfoForm?: NgForm) {
+    if(userinfoForm != null)
+      userinfoForm.reset();
+    this._userService.selectedUser = {
+      $key: null,
+      name: "",
+      position: "",
+      office: "",
+      salary: 0
+    }
   }
 
 }
